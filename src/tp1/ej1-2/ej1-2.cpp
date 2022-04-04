d114b79c6b4ca63976b17e199ff206b2dbdd6009
Juli <juliaventurini00@gmail.com>
2022-04-04 00:20:16 -0300

    agregamos struct actor y agregamos funciones a la clase red social

--- src/tp1/ej1-2/ej1-2.cpp
+++ src/tp1/ej1-2/ej1-2.cpp
@@ -7,10 +7,15 @@ RedSocial::RedSocial(std::string s)
 
 RedSocial::~RedSocial()
 {
-
 }
 
 string RedSocial::nombre() const
 {
 	return _nombreDelArchivo;
+}
+
+Actor::Actor(const int ID, const int inf)
+{
+	id = ID;
+	influencia = inf;
 }
--- src/tp1/ej1-2/ej1-2.h
+++ src/tp1/ej1-2/ej1-2.h
@@ -2,25 +2,36 @@
 #define REDSOCIAL
 
 #include "string"
+#include <vector>
 
 using namespace std;
 
 class RedSocial
 {
 public:
-
 	RedSocial(string nombreDelArchivo);
 
 	~RedSocial();
 
 	string nombre() const;
 
+	vector<Actor> actores;
+
+	//vector <Actor> amigos_de(int id); Funcion Auxiliar maybe?
+
+	vector<pair<Actor, Actor>> amistades;
+
 private:
 	string _nombreDelArchivo;
 };
 
+struct Actor
+{
+	Actor(const int ID, const int inf);
+
+	int id;
+
+	int influencia;
+};
+
 #endif // !REDSOCIAL
-
-
-
-
